9f2b6b368809b108d623683982f6b1b392853667
prettyannoying <skeny27@gmail.com>
2024-04-23 00:05:31 +0530

    see vue

---
+++ src/components/Balance.vue
@@ -0,0 +1,14 @@
+<template>
+    <h4>Your balance</h4>
+    <h1 id="balance">₹ {{ total }}</h1>
+</template>
+
+<script setup>
+import { defineProps } from 'vue';
+const props = defineProps ({
+    total: {
+        type: Number,
+        required: true
+    }
+})
+</script>
