d888eccb9599f888378679f27292a39d20a6b5a5
prettyannoying <skeny27@gmail.com>
2024-04-27 13:44:52 +0530

    i have mixed feelings about vue at the moment

--- src/components/TransactionList.vue
+++ src/components/TransactionList.vue
@@ -2,7 +2,7 @@
     <h3>History</h3>
     <ul id="list" class="list">
         <li v-for="transaction in transactions" v-bind:key="transaction.id" v-bind:class="transaction.amount < 0 ? 'minus' : 'plus'">
-            {{ transaction.text }} <span>₹{{ transaction.amount }}</span><button class="delete-btn">x</button>
+            {{ transaction.text }} <span>₹{{ transaction.amount }}</span><button @click="deleteTransaction(transaction.id)" class="delete-btn">x</button>
         </li>
         
     </ul>
@@ -16,5 +16,9 @@ const props = defineProps({
         required: true
     },
 });
+const emit = defineEmits(['transactionDeleted'])
+const deleteTransaction = (id) => {
+    emit('transactionDeleted', id);
+}
 </script>
 
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import './assets/style.css'
+import Toast from 'vue-toastification';
+import 'vue-toastification/dist/index.css';
+import { createApp } from 'vue';
+import App from './App.vue';
 
-import { createApp } from 'vue'
-import App from "./App.vue"
-
-createApp(App).mount('#app')
+const app = createApp(App);
+app.use(Toast);
+app.mount('#app');
